f1c3f83d9ea8303b512e56a3b7c3bc1a062d91ca
Gareth Cooper <gareth@garethcooper.com>
2014-03-13 12:12:43 +0000

    Wordrpess 3.9 : [responsiveimage] now rendered correctly when first added to editor

--- src/javascripts/responsiveImagePlugin.js
+++ src/javascripts/responsiveImagePlugin.js
@@ -6,65 +6,62 @@
  * Replaces [responsiveimage ... ] shortcodes with full-sized image in tinymce visual editor
  */
 tinymce.PluginManager.add('responsiveImagePlugin', function(editor, url) {
-
-  /**
-   * Fired when content is added to the tinymce editor
-   */
-  editor.onBeforeSetContent.add(function(ed, o) {
-    //console.log('beforesetcontent');
-
-    o.content = replaceResponsiveImageShortcodes(o.content);
-  });
-
-  /**
-   * Fired after content from tinymce editor is processed.
-   * i.e. when moving to html text editor or when content saved
-   */
-  editor.onPostProcess.add(function(ed, o) {
-    if (o.get)
-      o.content = restoreResponsiveImageShortcodes(o.content);
-  });
-
-  /**
-   * Replace [responsiveimage ... ] shortcodes with the placeholder image in the tinymce visual editor
-   * 
-   * @param {type} content The content of the tinymce editor
-   * @returns String The new content for the tinymceeditor
-   */
-  function replaceResponsiveImageShortcodes(content) {
-    return content.replace(/\[responsiveimage([^\]]*)\]/g, function(match, attrs) {
-      placeholder = getAttr(attrs, 'placeholder');
-
-      return '<img src="' + tinymce.documentBaseURL + '../wp-content/uploads/' + placeholder + '" class="responsiveimage mceItem" title="Responsive image" data-shortcode="responsiveimage' + tinymce.DOM.encode(attrs) + '" />';
-    });
-  }
-
-  /**
-   * Return the placeholder <img /> tags to [responsiveimage ... ] shortcodes
-   */
-  function restoreResponsiveImageShortcodes(content) {
-    return content.replace(/(?:<p[^>]*>)*(<img[^>]+>)(?:<\/p>)*/g, function(a, imageHtml) {
-      var cls = getAttr(imageHtml, 'class');
-
-      if (cls.indexOf('responsiveimage') !== -1)
-        return '[' + tinymce.trim(getAttr(imageHtml, 'data-shortcode')) + ']';
-
-      return a;
+    
+    /**
+     * Fired when content is added to the tinymce editor
+     */
+    editor.on('BeforeSetContent', function(event) {
+        event.content = replaceResponsiveImageShortcodes(event.content);
     });
 
-  }
+    /**
+     * Fired after content from tinymce editor is processed.
+     * i.e. when moving to html text editor or when content saved
+     */
+    editor.on('PostProcess', function(event) {
+        event.content = restoreResponsiveImageShortcodes(event.content);
+    });
 
-  /**
-   * Get the value of an attribute given an html-style string of attributes
-   * e.g. attr1="value" attr2="value2"
-   * 
-   * @param {type} s
-   * @param {type} n
-   * @returns {String}
-   */
-  function getAttr(s, n) {
-    n = new RegExp(n + '=\"([^\"]+)\"', 'g').exec(s);
-    return n ? tinymce.DOM.decode(n[1]) : '';
-  }
+    /**
+     * Replace [responsiveimage ... ] shortcodes with the placeholder image in the tinymce visual editor
+     * 
+     * @param {type} content The content of the tinymce editor
+     * @returns String The new content for the tinymceeditor
+     */
+    function replaceResponsiveImageShortcodes(content) {
+        return content.replace(/\[responsiveimage([^\]]*)\]/g, function(match, attrs) {
+            placeholder = getAttr(attrs, 'placeholder');
+
+            return '<img src="' + tinymce.documentBaseURL + '../wp-content/uploads/' + placeholder + '" class="responsiveimage mceItem" title="Responsive image" data-shortcode="responsiveimage' + tinymce.DOM.encode(attrs) + '" />';
+        });
+    }
+
+    /**
+     * Return the placeholder <img /> tags to [responsiveimage ... ] shortcodes
+     */
+    function restoreResponsiveImageShortcodes(content) {
+        return content.replace(/(?:<p[^>]*>)*(<img[^>]+>)(?:<\/p>)*/g, function(a, imageHtml) {
+            var cls = getAttr(imageHtml, 'class');
+
+            if (cls.indexOf('responsiveimage') !== -1)
+                return '[' + tinymce.trim(getAttr(imageHtml, 'data-shortcode')) + ']';
+
+            return a;
+        });
+
+    }
+
+    /**
+     * Get the value of an attribute given an html-style string of attributes
+     * e.g. attr1="value" attr2="value2"
+     * 
+     * @param {type} s
+     * @param {type} n
+     * @returns {String}
+     */
+    function getAttr(s, n) {
+        n = new RegExp(n + '=\"([^\"]+)\"', 'g').exec(s);
+        return n ? tinymce.DOM.decode(n[1]) : '';
+    }
 
 });
